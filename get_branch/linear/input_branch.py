93ee67737144a134f4f7a4e58b5f567c90c1a22e
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-03 23:50:19 +0900

    improve json structure

--- get_branch/linear/input_branch.py
+++ get_branch/linear/input_branch.py
@@ -3,8 +3,8 @@ import json
 
 def get_branch(layer_json_value):
     #入力層ノードの数はinput_featuresと同じ
-    number_of_node = layer_json_value["input_layer"]["input_features"]
+    number_of_node = layer_json_value["input_features"]
     #入力層ノードの次数はinput_layerのout_featuresと同じ
-    node_branch = layer_json_value["input_layer"]["out_features"]
+    node_branch = layer_json_value["out_features"]
     #key次数(横軸) valueノード数(縦軸) 
     return {node_branch:number_of_node}
