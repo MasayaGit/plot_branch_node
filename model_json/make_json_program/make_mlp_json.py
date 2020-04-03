18f0cc484c82996551e0d903e1d87b08fdd550b9
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-03 20:12:37 +0900

    make mlp json

--- model_json/make_json_program/make_mlp_json.py
+++ model_json/make_json_program/make_mlp_json.py
@@ -1,8 +1,13 @@
+# coding: UTF-8
 import json
 
+#モデル構造
+#https://medium.com/@aungkyawmyint_26195/multi-layer-perceptron-mnist-pytorch-463f795b897a
 def make_json():
     json_value = {
-        
+        "layer1":{"input_layer":{"input_features":784,"out_features":512}},
+        "layer2":{"hidden_layer":{"input_features":512,"out_features":512}},
+        "layer3":{"output_layer":{"input_features":512,"out_features":10}}
     } 
     with open('./model_json/json/mlp.json', 'w') as f:
         json.dump(json_value, f, ensure_ascii=False)
