d52a56aabae02e6f533f3b271481b36653f043a7
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-04 12:11:56 +0900

    add call hidden_branch

--- model_json/make_json_program/make_mlp_json.py
+++ model_json/make_json_program/make_mlp_json.py
@@ -5,9 +5,9 @@ import json
 #https://medium.com/@aungkyawmyint_26195/multi-layer-perceptron-mnist-pytorch-463f795b897a
 def make_json():
     json_value = {
-        "layer1":{"input_features":784,"out_features":512,"layer_type":"input"},
-        "layer2":{"input_features":512,"out_features":512,"layer_type":"hidden"},
-        "layer3":{"input_features":512,"out_features":10, "layer_type":"output"}
+        "layer0":{"input_features":784,"out_features":512,"layer_type":"input"},
+        "layer1":{"input_features":512,"out_features":512,"layer_type":"hidden"},
+        "layer2":{"input_features":512,"out_features":10, "layer_type":"output"}
     } 
     with open('/Users/info/Desktop/lab/program/plot_branch_node/model_json/json/mlp.json', 'w') as f:
         json.dump(json_value, f, ensure_ascii=False)
