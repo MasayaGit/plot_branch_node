5a500d778453b2706436eb14c97e41f33ca83d48
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-07 14:34:16 +0900

    add cnn get_branch

---
+++ forward/get_branch/cnn/cnn_hidden_branch.py
@@ -0,0 +1,12 @@
+# coding: UTF-8
+import json
+import numpy as np
+
+#画像は正方形を仮定
+def get_branch(layer_json_value,img_size):
+    #ノードの数は画像のピクセル数 (= 縦 * 横 * 入力画像のチャネル数)
+    number_of_node = img_size * img_size * layer_json_value["input_channels"]
+    #kernel_size * kernel_sizeだけそのピクセルは参照される それがout_channels(=フィルターの数と等価)だけ繰り返される
+    node_branch = layer_json_value["kernel_size"] * layer_json_value["kernel_size"] * layer_json_value["out_channels"]  
+    #np.full(10, 3) なら array([3, 3, 3, 3, 3, 3, 3, 3, 3, 3])
+    return np.full(number_of_node, node_branch)
--- forward/model_json/make_json_program/cnn/make_simple_cnn_json.py
+++ forward/model_json/make_json_program/cnn/make_simple_cnn_json.py
@@ -2,12 +2,13 @@
 import json
 
 #モデル構造
-#https://docs.microsoft.com/en-us/archive/msdn-magazine/2019/march/test-run-neural-regression-using-pytorch#training-the-model
+#https://github.com/pytorch/examples/blob/master/mnist/main.py
 def make_json():
     json_value = {
-        "layer0":{"input_features":13,"out_features":10,"layer_type":"input","processing_type":"linear"},
-        "layer1":{"input_features":10,"out_features":10,"layer_type":"hidden","processing_type":"linear"},
-        "layer2":{"input_features":10,"out_features":1, "layer_type":"output","processing_type":"linear"}
+        "layer0":{"input_channels":1, "out_channels":32,"kernel_size":3,"layer_type":"input","processing_type":"cnn"},
+        "layer1":{"input_channels":32, "out_channels":64,"kernel_size":3,"layer_type":"input","processing_type":"cnn"},
+        "layer2":{"input_features":9216,"out_features":128,"layer_type":"hidden","processing_type":"linear"},
+        "layer3":{"input_features":128,"out_features":10, "layer_type":"output","processing_type":"linear"}
     } 
-    with open('/Users/info/Desktop/lab/program/plot_branch_node/model_json/json/regression.json', 'w') as f:
+    with open('/Users/info/Desktop/lab/program/plot_branch_node/model_json/json/simple_cnn.json', 'w') as f:
         json.dump(json_value, f, ensure_ascii=False)
