e14915b3e184282cb7b5f563d6f7a6d3ad8e8db6
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-08 15:16:12 +0900

    add skip_connection_flag

--- forward/get_branch/cnn/cnn_output_branch.py
+++ forward/get_branch/cnn/cnn_output_branch.py
@@ -7,6 +7,8 @@ def get_branch(layer_json_value,img_size):
     #ノードの数は画像のピクセル数 (= 縦 * 横 * 入力画像のチャネル数)
     number_of_node = img_size * img_size * layer_json_value["input_channels"]
     #kernel_size * kernel_sizeだけそのピクセルは参照される それがout_channels(=フィルターの数と等価)だけ繰り返される
-    node_branch = layer_json_value["kernel_size"] * layer_json_value["kernel_size"] * layer_json_value["out_channels"]  
+    node_branch = layer_json_value["kernel_size"] * layer_json_value["kernel_size"] * layer_json_value["out_channels"]
+    if layer_json_value["skip_connection_flag"]:
+        raise Exception('Error!:cnn_output_branch do not get skip_connection_flag True')
     #np.full(10, 3) なら array([3, 3, 3, 3, 3, 3, 3, 3, 3, 3])
     return np.full(number_of_node, node_branch)
