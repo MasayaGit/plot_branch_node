9a70c0b058729e97f0021acde1fe7eb0882c3f39
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-08 17:14:26 +0900

    make adaptive_avg_pooling test json

---
+++ forward/model_json/make_json_program/cnn/test/make_simple_adaptive_avg_pooling_test_json.py
@@ -0,0 +1,19 @@
+# coding: UTF-8
+import json
+
+#モデル構造
+#https://github.com/pytorch/examples/blob/master/mnist/main.py
+def make_json():
+    json_value = {
+        "layer0":{"input_channels":3, "out_channels":5,"kernel_size":3,"layer_type":"input","processing_type":"cnn","skip_connection_flag":False},
+        "layer1":{"input_channels":5, "out_channels":7,"kernel_size":2,"layer_type":"hidden","processing_type":"cnn","skip_connection_flag":False},
+        
+        #画像サイズは output_size * output_sizeになる
+        "layer2":{"input_channels":7, "out_channels":7,"output_size":1,"layer_type":"adaptive_avg_pooling","processing_type":"cnn","skip_connection_flag":False},
+
+        #adaptive_avg_poolingによって、input_features = output_size * output_size * input_channels となる
+        "layer3":{"input_features":7,"out_features":4,"layer_type":"hidden","processing_type":"linear"},
+        "layer4":{"input_features":4,"out_features":2, "layer_type":"output","processing_type":"linear"}
+    } 
+    with open('/Users/info/Desktop/lab/program/plot_branch_node/forward/model_json/json/adaptive_avg_pooling_test.json', 'w') as f:
+        json.dump(json_value, f, ensure_ascii=False)
