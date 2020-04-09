603c6e8bc8a2c9c2170dda2adc8e26e122b0ea56
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-09 15:56:17 +0900

    make adaptive_avg_pooling test json

--- forward/model_json/make_json_program/cnn/test/make_simple_adaptive_avg_pooling_test_json.py
+++ forward/model_json/make_json_program/cnn/test/make_simple_adaptive_avg_pooling_test_json.py
@@ -9,7 +9,7 @@ def make_json():
         "layer1":{"input_channels":5, "out_channels":7,"kernel_size":2,"layer_type":"hidden","processing_type":"cnn","skip_connection_flag":False},
         
         #画像サイズは output_size * output_sizeになる
-        "layer2":{"input_channels":7, "out_channels":7,"output_size":1,"layer_type":"adaptive_avg_pooling","processing_type":"cnn","skip_connection_flag":False},
+        "layer2":{"input_channels":7, "out_channels":7,"output_size":1,"layer_type":"hidden","processing_type":"adaptive_avg_pooling","skip_connection_flag":False},
 
         #adaptive_avg_poolingによって、input_features = output_size * output_size * input_channels となる
         "layer3":{"input_features":7,"out_features":4,"layer_type":"hidden","processing_type":"linear"},
