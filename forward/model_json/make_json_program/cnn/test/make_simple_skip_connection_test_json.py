31772fab97386367871ec3cf28cd48b35e3405ae
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-11 23:32:39 +0900

    test skip connection is 2

--- forward/model_json/make_json_program/cnn/test/make_simple_skip_connection_test_json.py
+++ forward/model_json/make_json_program/cnn/test/make_simple_skip_connection_test_json.py
@@ -5,10 +5,11 @@ import json
 #https://github.com/pytorch/examples/blob/master/mnist/main.py
 def make_json():
     json_value = {
-        "layer0":{"input_channels":3, "out_channels":5,"kernel_size":3,"layer_type":"input","processing_type":"cnn","skip_connection":1},
+        "layer0":{"input_channels":3, "out_channels":5,"kernel_size":3,"layer_type":"input","processing_type":"cnn","skip_connection":2},
         "layer1":{"input_channels":5, "out_channels":7,"kernel_size":2,"layer_type":"hidden","processing_type":"cnn","skip_connection":0},
-        "layer2":{"input_features":63,"out_features":4,"layer_type":"hidden","processing_type":"linear"},
-        "layer3":{"input_features":4,"out_features":2, "layer_type":"output","processing_type":"linear"}
+        "layer2":{"input_channels":7, "out_channels":63,"kernel_size":3,"layer_type":"hidden","processing_type":"cnn","skip_connection":0},
+        "layer3":{"input_features":63,"out_features":4,"layer_type":"hidden","processing_type":"linear"},
+        "layer4":{"input_features":4,"out_features":2, "layer_type":"output","processing_type":"linear"}
     } 
     with open('/Users/info/Desktop/lab/program/plot_branch_node/forward/model_json/json/simple_skip_connection_test.json', 'w') as f:
         json.dump(json_value, f, ensure_ascii=False)
