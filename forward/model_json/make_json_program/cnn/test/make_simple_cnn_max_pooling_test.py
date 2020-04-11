87538f4389a635ee7b1d898aecbc5ca7bdd76471
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-12 00:24:22 +0900

    test max polling skip connection

--- forward/model_json/make_json_program/cnn/test/make_simple_cnn_max_pooling_test.py
+++ forward/model_json/make_json_program/cnn/test/make_simple_cnn_max_pooling_test.py
@@ -5,10 +5,10 @@ import json
 #https://github.com/pytorch/examples/blob/master/mnist/main.py
 def make_json():
     json_value = {
-        "layer0":{"input_channels":3, "out_channels":5,"kernel_size":3,"layer_type":"input","processing_type":"cnn","skip_connection":0},
+        "layer0":{"input_channels":3, "out_channels":5,"kernel_size":3,"layer_type":"input","processing_type":"cnn","skip_connection":3},
         #各ピクセルが一回だけ参照されるようなストライド、プーリングサイズを想定している。自分はストライドを指定する
-        "layer1":{"input_channels":5, "out_channels":5,"stride":2,"layer_type":"hidden","processing_type":"max_pooling","skip_connection":0},
-        "layer2":{"input_channels":5, "out_channels":7,"kernel_size":2,"layer_type":"hidden","processing_type":"cnn","skip_connection":0},
+        "layer1":{"input_channels":5, "out_channels":5,"stride":2,"layer_type":"hidden","processing_type":"max_pooling","skip_connection":2},
+        "layer2":{"input_channels":5, "out_channels":7,"kernel_size":2,"layer_type":"hidden","processing_type":"cnn","skip_connection":1},
         "layer3":{"input_features":63,"out_features":4,"layer_type":"hidden","processing_type":"linear"},
         "layer4":{"input_features":4,"out_features":2, "layer_type":"output","processing_type":"linear"}
     } 
