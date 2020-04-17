f2998fd114f89b862f31cd0eaa39f9ff96ebbbe1
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-17 14:02:50 +0900

    delte comment

--- forward/model_json/make_json_program/cnn/make_vgg16_json.py
+++ forward/model_json/make_json_program/cnn/make_vgg16_json.py
@@ -2,7 +2,6 @@
 import json
 
 #ImageNetの1000クラス 画像サイズは224×224を想定 ノード数がとても大きい数字になるから
-#MaxPool2dを適用した結果の画像のサイズは前の層と変わらないとする
 #モデル構造
 #http://aidiary.hatenablog.com/entry/20180212/1518404395
 def make_json():
