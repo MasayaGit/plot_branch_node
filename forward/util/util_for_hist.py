13f2d49265759fd1a2661a231031c58a93bc1765
MasayaGit <pontamasaya@i.softbank.jp>
2020-04-19 19:31:44 +0900

    improve hist

--- forward/util/util_for_hist.py
+++ forward/util/util_for_hist.py
@@ -7,6 +7,9 @@ def get_min_sabun(sort_set_branch):
         if i+1 == len(sort_set_branch):
             break
         current_sabun = sort_set_branch[i+1] - sort_set_branch[i]
+        #min_sabunが１0より小さいと、binsがとても多くなるので、continueする
+        if current_sabun < 10:
+            continue
         if i == 0:
             min_sabun = current_sabun
         if current_sabun < min_sabun:
@@ -16,9 +19,20 @@ def get_min_sabun(sort_set_branch):
 
 def get_bin(max_branch,min_sabun):
     bins = 0
-    #奇数ときは+1する。45 / 2 = 22.5。22.5なら２２分割ではなく、23分割にする 
-    if max_branch % min_sabun == 1:
+    #割り切れないときは+1する。45 / 2 = 22.5。22.5なら２２分割ではなく、23分割にする 
+    if max_branch % min_sabun != 0:
         bins = int(max_branch / min_sabun)+1
     else:
         bins = int(max_branch / min_sabun)
     return bins
+
+def get_sort_branch_list(branch_list):
+    set_branch_list = set(branch_list)
+    sort_set_branch_list = sorted(set_branch_list)
+    return sort_set_branch_list
+
+def get_max_set_branch(set_branch_list):
+    return max(set_branch_list)
+
+def get_min_set_branch(set_branch_list):
+    return min(set_branch_list)
